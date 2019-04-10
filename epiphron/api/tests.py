61126e1d0bfe4c2506f830444bbc285edd99c7f1
chehanr <chehan.rat@gmail.com>
2019-04-10 23:00:09 +1000

    Added basic test cases.

--- epiphron/api/tests.py
+++ epiphron/api/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
 from ..core.models import Category, Product, Seller
 
@@ -7,34 +9,71 @@ class APITestCase(TestCase):
 
     """
     Core API tests.
-    TODO Implement.
     """
 
     def setUp(self):
+        self.client = APIClient()
+
         self.seller = Seller()
+        self.seller = Seller(name="TestSeller")
+        self.seller.save()
+
         self.category = Category()
+        self.category = Category(name="TestCategory")
+        self.category.save()
+
         self.product = Product()
+        self.product = Product(seller=self.seller,
+                               category=self.category,
+                               name="TestProduct")
+        self.product.save()
 
     def test_can_get_seller_list(self):
         """GET /sellers returns a list of sellers."""
-        assert False is True
+        url = reverse('api-seller-list')
+        response = self.client.get(url)
+        assert response.status_code == 200, "Expect 200 OK. got: {}".format(
+            response.status_code)
 
     def test_can_get_seller_item(self):
         """GET /sellers/item/{pk} returns a seller."""
-        assert False is True
+        url = reverse('api-seller-item', args=[self.seller.pk])
+        response = self.client.get(url)
+        assert response.status_code == 200, "Expect 200 OK. got: {}".format(
+            response.status_code)
 
     def test_can_get_category_list(self):
         """GET /categories returns a list of categories."""
-        assert False is True
+        url = reverse('api-category-list')
+        response = self.client.get(url)
+        assert response.status_code == 200, "Expect 200 OK. got: {}".format(
+            response.status_code)
 
     def test_can_get_category_item(self):
         """GET /categories/item/{pk} returns a category."""
-        assert False is True
+        url = reverse('api-category-item', args=[self.category.pk])
+        response = self.client.get(url)
+        assert response.status_code == 200, "Expect 200 OK. got: {}".format(
+            response.status_code)
 
     def test_can_get_product_list(self):
         """GET /products returns a list of products."""
-        assert False is True
+        url = reverse('api-product-list')
+        response = self.client.get(url)
+        assert response.status_code == 200, "Expect 200 OK. got: {}".format(
+            response.status_code)
 
     def test_can_get_product_item(self):
         """GET /products/item/{pk} returns a product."""
-        assert False is True
+        url = reverse('api-product-item', args=[self.product.pk])
+        response = self.client.get(url)
+        assert response.status_code == 200, "Expect 200 OK. got: {}".format(
+            response.status_code)
+
+    def tearDown(self):
+        for seller in Seller.objects.all():
+            seller.delete()
+        for category in Category.objects.all():
+            category.delete()
+        for product in Product.objects.all():
+            product.delete()
